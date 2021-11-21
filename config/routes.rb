5362cc90eb5f04b3a049fc209a9b9c9d844fd811
Keenah <kvancamp@mail.com>
2021-11-21 07:53:08 -0600

    Fixed layout
    Managed to mess up the layout again

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get '/dashboard' => 'dashboard#index', as: :dashboard
-  get 'dashboard/properties'
-  get 'properties/new' 
-  get 'dashboard/reports'
+  resources :properties
   devise_for :accounts
+
+  get '/dashboard' => 'dashboard#index', as: :dashboard
+  #get 'dashboard/properties'
+  #get '/properties/new' => 'properties#new'
+  #get 'dashboard/reports'
   root to: 'public#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
