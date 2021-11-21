de4157fc7428bac9f86359c96ba9194ff5e7bf4e
Keenah <kvancamp@mail.com>
2021-11-21 02:28:20 -0600

    Update Properties_controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,31 @@
 class PropertiesController < ApplicationController
+    before_action :set_property, only: [:show, :edit, :update, :destroy]
 
+    def index
+        @properties = Property.all
+    end
+
+    def show
+    end
+
+    def new
+        @property = Property.new
+    end
+
+    def edit
+    end
+
+    def create
+        @property = Property.new(property_params)
+
+        respond_to do |format|
+            if @property.save 
+                format.html { redirect_to @property, notice: 'Success!.'}
+                format.json { render :show, status: :created, location: @property}
+            else
+                format.html { render :new}
+                format.json { render json: @property.errors, status: :unprocessable_entity}
+            end
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/dashboard' => 'dashboard#index', as: :dashboard
-  get 'dashboard/properties'
+  get 'dashboard/properties' 
   get 'dashboard/reports'
   devise_for :accounts
   root to: 'public#main'
