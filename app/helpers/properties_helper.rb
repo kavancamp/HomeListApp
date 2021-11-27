b4a939b9f9e5a8653f5c6178fd912c82aed51e89
Keenah <kvancamp@mail.com>
2021-11-27 04:30:29 -0600

    added newest property display
    Added a new section on the home page, utilized a helper method and Loaded in the latest properties to the home page

---
+++ app/helpers/properties_helper.rb
@@ -0,0 +1,8 @@
+module PropertiesHelper
+
+    def property_thumbnail
+        img = property.photo.present ? property.photo.thumb.url : "placeholder.png"
+        image_tag img, class: "property-thumb"
+    end
+
+end
