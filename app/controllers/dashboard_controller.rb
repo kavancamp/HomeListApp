cb2cf1b09815cd8fcbfc042b1807b4e957a26a22
Keenah <kvancamp@mail.com>
2021-11-28 09:38:08 -0600

    Update dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
-  before_action :set_sidebar, except: [:show]
-  
+  before_action :set_sidebar
+
   def index
   end
 
@@ -12,12 +12,6 @@ class DashboardController < ApplicationController
     @properties_for_sale = Property.where(account_id: @account.id).for_sale
     @properties_leased = Property.where(account_id: @account.id).leased
     @properties_for_rent = Property.where(account_id: @account.id).for_rent
-  end
-
-  def properties
-  end
-
-  def reports
   end
 
   private
