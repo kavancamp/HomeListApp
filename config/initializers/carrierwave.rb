84e6a5f902a4a5dbf34bd58bba57b9eb222ace68
Keenah <kvancamp@mail.com>
2021-11-28 06:51:57 -0600

    removed btn-block

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config| 
     config.fog_credentials = { 
-      :provider               => 'AWS', 
-      :aws_access_key_id      ENV['AWS_ACCESS_KEY_ID'], 
-      :aws_secret_access_key  ENV['AWS_SECRET_ACCESS_KEY'], 
-    } 
+      provider:                'AWS', 
+      aws_access_key_id:        ENV['AWS_ACCESS_KEY_ID'], 
+      aws_secret_access_key:    ENV['AWS_SECRET_ACCESS_KEY'], 
+    }
     config.storage = :fog
     config.permissions = 0666
     config.cache_dir ="#{Rails.root}/tmp/"
