278f7345560319087ad15586353dbab748387645
Keenah <kvancamp@mail.com>
2021-12-03 08:57:46 -0600

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = { 
     :provider               => 'AWS', 
     :region                 => 'us-east-2',
-    :aws_access_key_id      => 'AKIASW7XMGNKPSZNZFOB', 
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => '', 
+    :aws_secret_access_key  => '',
 
     }
     config.fog_directory  = 'flatironprojectbucket27' 
