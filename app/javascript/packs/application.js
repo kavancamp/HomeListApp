6c1e7dd9bb974baf915a725597c2de64a2f7628e
Keenah <kvancamp@mail.com>
2021-12-09 09:14:31 -0600

    Added webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 require("@rails/ujs").start()
-require("includes/properties");
+require("jquery");
+require("./includes/properties");
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
