c334daacc4cdddf12c4b8ee0c1066308374fe461
Keenah <kvancamp@mail.com>
2021-12-09 04:15:40 -0600

    Migrated a description section
    and worked on formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_085831) do
+ActiveRecord::Schema.define(version: 2021_12_09_092905) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_085831) do
     t.integer "account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
