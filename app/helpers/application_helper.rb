fe011910bde7bcf45d03bb746b3546df2fbe68af
Keenah <kvancamp@mail.com>
2021-12-30 15:04:16 -0600

    Fixed parameters
    Also added account photo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def profile_picture account, width = 150
+        thumb = account.image.present? ? account.image.thumbnail.url : "placeholder.jpg"
+        image_tag thumb, width: width, class: "profile-pic img-round"
+
+     end
 end
