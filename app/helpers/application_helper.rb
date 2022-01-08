1632405d1cc9d41fcab40e5f69297d8a79081dfc
Keenah <kvancamp@mail.com>
2022-01-07 18:22:42 -0600

    fixed layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
-    def profile_picture account, width = 150
-        thumb = account.image.present? ? account.image.thumbnail.url : "placeholder.jpg"
-        image_tag thumb, width: width, class: "profile-pic img-round"
 
-     end
+    #def profile_picture account, width = 150
+        #thumb = account.photo.present? ? account.image.thumbnail.url : "placeholder.jpg"
+        #image_tag thumb, width: width, class: "profile-pic img-round"
+
+     #end
 end
