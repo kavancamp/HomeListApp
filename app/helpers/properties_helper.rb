f9317f3ba71ec626be1fafeca27ce5e361f30ad6
Keenah <kvancamp@mail.com>
2022-02-13 14:20:08 -0600

    Troubleshooting Helper

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,15 +1,12 @@
-@ -1,15 +1,17 @@
 module PropertiesHelper
 
-  def property_thumbnail property
-    img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
-    image_tag img, class: "property-thumb"
-
-  def property_photo_url property
-    img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+  def property_thumbnail(property)
+    photo = property.photo.present? ? property.photo. : "placeholder.png"
+    image_tag photo, class: "property-thumb"
   end
 
-  def property_thumbnail_url property
-    property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+  def property_photo_url(property)
+    property.photo.present? ? polymorphic_url(property.photo) : asset_url("/placeholder.png")
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :accounts
   resources :properties
-  root to: 'public#main'
+  root "public#main"
   
   get "/accounts" => 'admin#accounts', as: :accounts
   get "/dashboard" => 'dashboard#index', as: :dashboard
-  
+
+  devise_scope :user do
+    get '/accounts/sign_out' => 'devise/sessions#destroy', method: :delete
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
