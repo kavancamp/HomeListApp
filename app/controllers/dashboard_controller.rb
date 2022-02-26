c7a0dbfc9436d07ec34bbef4d942414302de1342
Keenah <kvancamp@mail.com>
2022-02-25 19:02:43 -0600

    Removed placeholder and replaced

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,19 +4,10 @@ class DashboardController < ApplicationController
   def index
   end
 
-  def profile
-    @account = Account.find(params[:id])
-    @properties = Property.where(account_id: @account.id)
-    
-    @properties_sold = Property.where(account_id: @account.id).sold
-    @properties_for_sale = Property.where(account_id: @account.id).for_sale
-    @properties_leased = Property.where(account_id: @account.id).leased
-    @properties_for_rent = Property.where(account_id: @account.id).for_rent
-  end
-
   private
 
   def set_sidebar
     @show_sidebar = true
   end
+  
 end
