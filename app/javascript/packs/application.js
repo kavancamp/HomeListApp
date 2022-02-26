e58253e8766aabcac500a9f3438037022172f4cf
Keenah <kvancamp@mail.com>
2022-02-25 19:04:19 -0600

    Updated application layout and link to property

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,57 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 require("@rails/ujs").start()
+require("./includes/properties");
+require("jquery");
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap")
 
-var jQuery = require("jquery")
+var jquery = require("jquery")
 global.$ = global.jQuery = jQuery
 window.$ = window.jQuery = jQuery
 
-require("bootstrap")
-require("fontawesome-free");
-require("./includes/properties");
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()                                    
-ActiveStorage.start()
-'use strict'
-
-const AWS = require('aws-sdk')
-AWS.config.update({ region: process.env.AWS_REGION })
-const s3 = new AWS.S3()
-
-const URL_EXPIRATION_SECONDS = 300
-
-
-exports.handler = async (event) => {
-  return await getUploadURL(event)
+import toastr from 'toastr';
+toastr.options = {
+    progressBar: true
 }
-
-const getUploadURL = async function(event) {
-  const randomID = parseInt(Math.random() * 10000000)
-  const Key = `${randomID}.jpg`
+global.toastr = toastr;
 
 
-  const s3Params = {
-    Bucket: process.env.UploadBucket,
-    Key,
-    Expires: URL_EXPIRATION_SECONDS,
-    ContentType: 'image/jpeg',
-
-   ACL: 'public-read'
-  }
-
-  console.log('Params: ', s3Params)
-  const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params)
-
-  return JSON.stringify({
-    uploadURL: uploadURL,
-    Key
-  })
-
-  
-  
