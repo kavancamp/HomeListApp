2d5be54a9038d21bee57f242402af614460be813
Keenah <kvancamp@mail.com>
2022-03-02 23:04:51 -0600

    Added scopes

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,15 +4,11 @@ class Property < ApplicationRecord
   belongs_to :account
   has_many_attached :photo
  
- 
-  #latest 
   scope :latest, -> { order created_at: :desc }
-
-  scope :sold, -> { where for_sale: true, status: "sold" }
-  scope :for_sale, -> { where for_sale: true, status: "avaliable" }
-  scope :leased, -> { where for_sale: false, status: "leased" }
-  scope :for_rent, -> { where for_sale: false, status: "avaliable" }
-  scope :avaliable, -> { where status: "avaliable" }
+  scope :sold, -> { where for_sale: true, status: "Sold" }
+  scope :for_sale, -> { where for_sale: true, status: "Avaliable" }
+  scope :for_rent, -> { where for_sale: false, status: "Avaliable" }
+  scope :avaliable, -> { where status: "Avaliable" }
   
   validates :name, presence: true, length: {minimum: 3, maximum: 50}
   validates :address, presence: true, length: {minimum: 3, maximum: 100}
