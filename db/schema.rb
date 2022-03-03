6e5af003c90f16fe31e24e8fbcceb9f005615a22
Keenah <kvancamp@mail.com>
2022-03-02 23:11:10 -0600

    migration
    phone number added to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_050103) do
+ActiveRecord::Schema.define(version: 2022_03_03_050729) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_050103) do
     t.string "company"
     t.string "profile_picture"
     t.string "company_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
