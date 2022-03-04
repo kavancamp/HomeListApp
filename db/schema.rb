04649927bd8ee42be182e01114bc59868db8c10d
Keenah <kvancamp@mail.com>
2022-03-03 18:18:27 -0600

    Update Show.html & migration
    Added availability to houses and added agent information to show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_222157) do
+ActiveRecord::Schema.define(version: 2022_03_03_225036) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_222157) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.boolean "for_sale", default: true
+    t.datetime "available_date"
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
