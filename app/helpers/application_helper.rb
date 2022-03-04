c60ffff8800efebda8709424268ec711c177c9d3
Keenah <kvancamp@mail.com>
2022-03-03 20:48:13 -0600

    removed styling for profile picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 
      def profile_picture(account)
         account.profile_picture.present? ? polymorphic_url(account.profile_picture) : asset_url("avatar_placeholder.jpeg")
-        image_tag thumb, width: width, class: "profile-pic img-round"
     end
 
 end
