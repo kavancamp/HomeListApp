3723d75ad06e04d148e13c99ee02eb8e12b77d51
Keenah <kvancamp@mail.com>
2022-03-09 10:38:57 -0600

    Create messages_controller.rb

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,29 @@
+class MessagesController < ApplicationController
+    before_action :set_sidebar
+    before_action do
+        @conversation = Conversation.find(params[:conversation_id])
+      end
+
+    
+      def index
+        @messages = @conversation.messages
+        @message = @conversation.messages.new
+      end
+      def new
+        @message = @conversation.messages.new
+      end
+    
+      def create
+        @message = @conversation.messages.new(message_params)
+        @message.account_id = current_account.id
+        if @message.save
+          redirect_to conversation_messages_path(@conversation)
+        end
+      end
+  
+      private
+      def message_params
+        params.require(:message).permit(:body, :account_id)
+      end
+      
+end
