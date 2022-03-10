090b65ad692feabf352e1618873ef70fc9c92f9a
Keenah <kvancamp@mail.com>
2022-03-09 20:34:09 -0600

    Update application_helper.rb
    added profile picture to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-
-     def profile_picture(account)
-        account.profile_picture.present? ? polymorphic_url(account.profile_picture) : asset_url("avatar_placeholder.jpeg")
+    def profile_picture(account)
+        account.profile_picture.present? ? polymorphic_url(account.profile_picture) : asset_url("profile_picture_placeholder.jpeg")
     end
-
+    
 end
