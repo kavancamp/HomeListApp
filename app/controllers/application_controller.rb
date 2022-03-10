70c94d5e5b74dd235055f35d92e96f52546aad0d
Keenah <kvancamp@mail.com>
2022-03-09 20:31:44 -0600

    Update application_controller.rb
    added :role to param sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :url, :profile_picture, :image_cache, :company, :phone_number, :email ])
-        devise_parameter_sanitizer.permit(:sign_up,  keys: [:first_name, :last_name]) 
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :url, :profile_picture, :image_cache, :company, :phone_number, :email, :role ])
+        devise_parameter_sanitizer.permit(:sign_up,  keys: [:first_name, :last_name, :role, :phone_number]) 
     end
     
 end
