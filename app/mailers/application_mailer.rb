293d71a2ae52f601db585e2ae15f53b5ee255275
Keenah <kvancamp@mail.com>
2022-03-09 20:35:33 -0600

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,17 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def email_agent(agent_id, name, email, message)
+
+    @agent = Account.find(agent_id)
+    @name = name
+    @email = email
+    @message = message
+
+    if @agent.present?
+        mail to: @agent.email, message:"Enquiry about one of your listings!"
+    end
+end
+
 end
