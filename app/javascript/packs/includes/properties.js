ffef5b41c7f8c80a4773cc249c493796920231c3
Keenah <kvancamp@mail.com>
2022-03-09 20:34:25 -0600

    Create properties.js
    created js file

---
+++ app/javascript/packs/includes/properties.js
@@ -0,0 +1,45 @@
+$(function() {
+    $("#show-number").on("click", function(){
+        var $this = $(this);
+        $this.find("span").text($this.data("number"));  
+    });
+
+
+    $("#send-email-to-agent").on("click", function(){
+        const agentId = $("#agent_id").val();
+        const senderName = $("#sender-name").val();
+        const senderEmail = $("#sender-email").val();
+        const senderMessage = $("#sender-message").val();
+        $.ajax({
+            url: "/agent/email",
+            method: "POST",
+            dataType: "JSON",
+            data: {
+                agent_id: agentId,
+                sender_name: senderName,
+                sender_email: senderEmail,
+                sender_message: senderMessage
+
+            },
+            success: function(data){
+                $("#contact-modal form").remove();
+                $("#send-email-to-agent").remove();
+                $("#contact-modal").html("<p>Your message has been successfully sent!</p>");
+    
+            }
+        });
+    });
+
+
+
+
+    var exampleModal = document.getElementById('email-agent')
+    exampleModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget
+        var recipient = button.getAttribute('data-bs-whatever')
+        var modalTitle = exampleModal.querySelector('.modal-title')
+        var modalBodyInput = exampleModal.querySelector('.modal-body input')
+    });
+
+
+});
