fda30a839d47f2139a8ec5a32ada41b490e6ea62
Keenah <kvancamp@mail.com>
2022-03-10 20:15:46 -0600

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,5 @@ module ApplicationHelper
     def profile_picture(account)
         account.profile_picture.present? ? polymorphic_url(account.profile_picture) : asset_url("profile_picture_placeholder.jpeg")
     end
-    
+
 end
