664abc3042552f66cf09a40c26e2cdb34ff59ceb
Keenah <kvancamp@mail.com>
2022-03-10 20:14:04 -0600

    no change

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,11 +5,12 @@ class Property < ApplicationRecord
   has_one_attached :photo
  
   scope :latest, -> { order created_at: :desc }
-  scope :sold, -> { where for_sale: true, status: "Sold" }
-  scope :for_sale, -> { where for_sale: true, status: "Avaliable" }
-  scope :for_rent, -> { where for_sale: false, status: "Avaliable" }
-  scope :avaliable, -> { where status: "Avaliable" }
-  
+  scope :sold, -> { where for_sale: true, status: "sold" }
+  scope :for_sale, -> { where for_sale: true, status: "avaliable" }
+  scope :leased, -> { where for_sale: false, status: "leased" }
+  scope :for_rent, -> { where for_sale: false, status: "avaliable" }
+  scope :avaliable, -> { where status: "avaliable" }
+
   validates :name, presence: true, length: {minimum: 3, maximum: 50}
   validates :address, presence: true, length: {minimum: 3, maximum: 100}
   
