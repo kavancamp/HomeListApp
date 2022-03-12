25ed23810be081cee70f3a20739ac17a67e5f43e
Keenah <kvancamp@mail.com>
2022-03-11 18:48:07 -0600

    Update conversation.rb

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,6 +3,7 @@ class Conversation < ActiveRecord::Base
     belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'Account'
   
     has_many :messages, dependent: :destroy
+    
     validates_uniqueness_of :sender_id, :scope => :recipient_id
   
     scope :between, -> (sender_id,recipient_id) do
