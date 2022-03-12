eaf92f1c046c40b360aabe0817856b16cee250a5
Keenah <kvancamp@mail.com>
2022-03-11 23:08:12 -0600

    no change

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
     belongs_to :conversation
     belongs_to :account
-  
+    has many :conversations, through: :account
+
     validates_presence_of :body, :conversation_id, :account_id
 end
