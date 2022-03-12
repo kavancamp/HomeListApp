02dd081206967425b8306d097b0fe864b0a56acc
Keenah <kvancamp@mail.com>
2022-03-11 18:48:00 -0600

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,8 +4,9 @@ class Account < ApplicationRecord
 
   has_one_attached :profile_picture 
   has_many :properties
-  has_many :photo, :through => :properties
-  
+  has_many :photos, through: :properties
+  has_many :messages, through: :conversations
+
   def full_name
     "#{first_name} #{last_name}"
   end
