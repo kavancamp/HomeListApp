c0c8f51fe60168a0ba791dd3d1b3860c0e077096
Keenah <kvancamp@mail.com>
2022-03-13 17:09:27 -0500

    Update properties_helper.rb
    added repayment calculator

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -9,4 +9,29 @@ module PropertiesHelper
     property.photo.present? ? polymorphic_url(property.photo) : asset_url("property_banner_placeholder.png")
   end
 
+  def display_price(price)
+    display_amnt = number_to_currency(price, :precision => 0) 
+  end
+
+  def calculate_repayment(price)
+      downpayment = price * 0.20
+      price = price - downpayment
+      repayment = price / 360
+      repayment = display_price(repayment)
+  end
+
+
+
+  def get_property_price(property_price)
+      "Contact Agent" and return if property_price.nil? 
+      if property_price > 0
+          if @property.for_sale
+              display_price = display_price(property_price)
+          else
+              display_price = "#{display_price(property_price)} / Month"
+          end
+      else 
+          display_price = "Contact Agent"
+      end
+  end
 end
