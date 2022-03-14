11f7ef4617219d88a78c7919494d86e553653f34
Keenah <kvancamp@mail.com>
2022-03-14 14:23:06 -0500

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@rails/ujs").start()
-require("./includes/properties");
 require("jquery");
+require("@rails/ujs").start()
+require("bootstrap-sprockets")
+require("./includes/properties");
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap")
+
 
 var jquery = require("jquery")
 global.$ = global.jQuery = jQuery
